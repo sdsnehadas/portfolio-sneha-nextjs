297c8837a368817deada42c9f1308e3e6bc29a2c
sdsnehadas <sdsnehadas5@gmail.com>
2025-03-09 23:59:41 -0400

    Next JS portfolio initial commit

---
+++ components/Resume.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Resume = () => {
+  return (
+    <div className="w-full pt-20 pb-10" id="resume">
+      
+    </div>
+  );
+}
+
+export default Resume;
+
---
+++ data/index.ts
@@ -0,0 +1,164 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+  { name: "Resume", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I enjoy working in a collaborative environment",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Cloth Canvas : Ecommerce application using React",
+    des: "A place where you shop ",
+    img: "/clothCanvas.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/redux.svg"],
+    link: "https://github.com/sdsnehadas/Cloth-canvas",
+  },
+  {
+    id: 2,
+    title: "LET'S EAT : Food Delivery App",
+    des: "Food Delivery Application using React 18",
+    img: "/letseat.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/redux.svg"],
+    link: "https://github.com/sdsnehadas/react-from-scratch/tree/master",
+  },
+  {
+    id: 3,
+    title: "ClickFlix: NetFlix clone using React",
+    des: "Netflix GPT using React 18",
+    img: "/netflix1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/redux.svg"],
+    link: "https://clickflix-gpt.web.app/",
+  },
+  {
+    id: 4,
+    title: "Homes App Angular 16",
+    des: "Real Estate Web application using Angular 16",
+    img: "/HomesApp.png",
+    iconLists: ["/angular.svg", "/tail.svg", "/ts.svg"],
+    link: "https://github.com/sdsnehadas/angular-homes",
+  },
+  ,
+  {
+    id: 5,
+    title: "Showtube : Youtube clone using React",
+    des: "Welocme to Showtube",
+    img: "/show-tube.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/redux.svg"],
+    link: "https://github.com/sdsnehadas/show-tube",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Sneha was always able to find innovative solutions to complex technical problems. She has a strong ability to work well under pressure and handle multiple projects. She was also able to effectively communicate technical information to non-technical persons, making her a valuable asset to any team.",
+    name: "Shinija Mohan",
+    title: "Assosiate Principle Engineer at QBurst",
+  },
+  {
+    quote:
+      "Sneha has demonstrated remarkable leadership abilities as a lead engineer, where she mentored and guided the team while fostering collaboration and continuous improvement. Her problem-solving skills are top-notch, and she has a keen attention to detail, always ensuring that the final product meets the highest standards of performance, scalability, and accessibility.",
+    name: "Sreejith Sreedharan",
+    title: "Software Engineering Specialist",
+  },
+  {
+    quote:
+      "I had the pleasure of working with Sneha during our time together at QBurst, and I can confidently say that she is an exceptional professional with remarkable skills in frontend development. With over 9 years of experience in the field, Sneha  possesses deep expertise in technologies like React, Angular, and Node.js. She is also highly proficient in using modern frameworks and libraries, as well as backend development, which enables her to handle complex full-stack projects with ease.",
+    name: "Sujay Das",
+    title: "Senior Associate at Fannie Mae",
+  },
+];
+
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Software Developer",
+    desc: "Developed and automated API testing with Postman, integrated tests into CI/CD pipelines for continuous deployment, implemented data-driven testing, and mentored team members on best practices.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "FRONTEND JAVASCRIPT DEVELOPER- ANGULAR",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.Developed and optimized web applications using Angular, JavaScript ES6+, and Redux, migrated legacy jQuery code, ensured cross-browser compatibility, integrated RESTful APIs, built reusable Material-UI components, followed Agile and DevOps practices with CI/CD, and led pair programming sessions for team collaboration.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: " SENIOR ENGINEER - REACT DEVELOPER",
+    desc: "Developed modular and reusable React components, implemented state management with Redux Toolkit and Context API, optimized performance using lazy loading and code splitting, maintained and integrated legacy jQuery code, built high-performance eCommerce front-ends, conducted code reviews, wrote unit tests with Jest, collaborated with designers for UX improvements, and configured Webpack for optimized builds.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/sdsnehadas",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/snehacdas/",
+  },
+];
